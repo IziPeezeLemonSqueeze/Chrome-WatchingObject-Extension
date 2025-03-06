9f498662cd96955ea69c25dea103b7f1b226ce34
Stefano Pastore <65863208+IziPeezeLemonSqueeze@users.noreply.github.com>
2025-03-06 13:24:26 +0100

    update

--- src/utils_content/dcSnippet.ts
+++ src/utils_content/dcSnippet.ts
@@ -3,34 +3,41 @@ export class DCSNIPPET
 	consoleIntervalSearch: NodeJS.Timeout | undefined;
 	frameSnippet: HTMLIFrameElement;
 	divCodeSnippet: HTMLDivElement;
+	divMenuBar: HTMLDivElement;
 	isFrameClosed: boolean = false;
 
 	devConsoleTool()
 	{
 		console.log('DEV CONSOLE TOOL')
-		const editorBody = document.getElementById('panel-1136');
-		if (!editorBody)
+
+		const parentDivMenuBar = document.getElementById('toolbar-1043-targetEl');
+		this.divMenuBar = document.createElement('div');
+		this.divMenuBar.setAttribute('class', "x-btn x-box-item x-toolbar-item x-item-disabled x-btn-default-toolbar-small x-disabled x-btn-disabled x-btn-default-toolbar-small-disabled x-noicon x-btn-noicon x-btn-default-toolbar-small-noicon pulse-button");
+		this.divMenuBar.setAttribute('style', "border-width: 1px; left: 332px; top: 2px; margin: 0px; cursor: pointer");
+		this.divMenuBar.innerText = 'CODE SNIPPET';
+
+		this.divMenuBar.animate(
+			[
+				{ boxShadow: '0 0 15px rgba(0, 170, 255, 0.76)' },
+				{ boxShadow: '0 0 20px rgba(0, 170, 255, 1)' },
+				{ boxShadow: '0 0 5px rgba(0, 170, 255, 0.5)' }
+			],
+			{
+				duration: 2000,
+				iterations: Infinity,
+				easing: 'ease-in-out'
+			}
+		);
+
+		this.divMenuBar.addEventListener('click', () =>
 		{
-			console.log('editorBody NOT FOUND');
-			return;
-		}
+			this.startFrame();
+		});
 
-		const parentElement = editorBody.parentElement;
+		parentDivMenuBar.appendChild(this.divMenuBar);
 
-		this.divCodeSnippet = document.createElement('div');
-		this.divCodeSnippet.setAttribute('style', 'left: 0%;bottom: 0%;position: absolute;z-index: 1000;width: -webkit-fill-available;');
-
-		this.frameSnippet = document.createElement('iframe');
-		this.frameSnippet.setAttribute('style', 'background: white;position: fixed;bottom: 0%;width: -webkit-fill-available;');
-		this.frameSnippet.src = chrome.runtime.getURL('developerConsoleSnippet.html');
-
-		this.divCodeSnippet.appendChild(this.frameSnippet);
-
-		parentElement.appendChild(this.divCodeSnippet);
 		this.isFrameClosed = false;
 		clearInterval(this.consoleIntervalSearch);
-
-		this.initCompositionSnippet();
 	}
 
 	changeHeight()
@@ -84,6 +91,26 @@ export class DCSNIPPET
 			console.log('closing')
 		}
 	}
+
+	startFrame()
+	{
+		const editorBody = document.getElementById('panel-1136');
+		if (!editorBody)
+		{
+			console.log('editorBody NOT FOUND');
+			return;
+		}
+		const parentElement = editorBody.parentElement;
+
+		this.frameSnippet = document.createElement('iframe');
+		this.frameSnippet.id = 'DCSFRAME';
+		this.frameSnippet.src = chrome.runtime.getURL('developerConsoleSnippet.html');
+		this.frameSnippet.setAttribute('class', 'iframe');
+		this.frameSnippet.setAttribute('style', "z-index: 1000;position: absolute;width: 56.5%;height: 500px;top: 5%;left: 23%;border: none !important;outline: none!important;")
+
+		parentElement.appendChild(this.frameSnippet);
+	}
+
 
 	initCompositionSnippet()
 	{
